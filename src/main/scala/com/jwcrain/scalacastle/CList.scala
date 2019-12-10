96ce2014674e4a6d5211fdb712506fcd92407d05
crain9412 <crain@gomerits.com>
2019-12-09 16:48:55 -0800

    Add tail, drop, dropWhile, push, reverse, and init to CList

--- src/main/scala/com/jwcrain/scalacastle/CList.scala
+++ src/main/scala/com/jwcrain/scalacastle/CList.scala
@@ -1,8 +1,23 @@
 package com.jwcrain.scalacastle
 
-sealed trait CList[+T]
-case object Nil extends CList[Nothing]
-case class Node[+T](head: T, tail: CList[T]) extends CList[T]
+import scala.annotation.tailrec
+
+sealed trait CList[+T] {
+  def head: T
+  def tail: CList[T]
+  def isEmpty: Boolean
+}
+case object Nil extends CList[Nothing] {
+  override def head: Nothing = throw new NoSuchElementException("head of empty list")
+  override def tail: Nothing = throw new NoSuchElementException("tail of empty list")
+  override def isEmpty = true
+}
+
+case class Node[+T](hd: T, tl: CList[T]) extends CList[T] {
+  override def head: T = hd
+  override def tail: CList[T] = tl
+  override def isEmpty = false
+}
 
 object CList {
   def sum(ints: CList[Int]): Int = ints match {
@@ -13,6 +28,58 @@ object CList {
   def product(doubles: CList[Double]): Double = doubles match {
     case Nil => 1.0
     case Node(head, tail) => head * product(tail)
+  }
+
+  def tail[T](list: CList[T]): CList[T] = {
+    if (list.isEmpty) Nil
+    list.tail
+  }
+
+  def drop[T](list: CList[T], n: Int): CList[T] = {
+    @tailrec def go(i: Int, currentList: CList[T]): CList[T] = {
+      if (currentList.isEmpty) Nil
+      if (i == n) return currentList
+      go (i + 1, tail(currentList))
+    }
+
+    go(0, list)
+  }
+
+  def dropWhile[T](list: CList[T], f: T => Boolean): CList[T] = {
+    @tailrec def go(currentList: CList[T]): CList[T] = {
+      if (currentList.isEmpty) Nil
+      if (!f(currentList.head)) return currentList
+      go (tail(currentList))
+    }
+
+    go(list)
+  }
+
+  def push[T](list: CList[T], node: T): CList[T] = {
+    Node(node, list)
+  }
+
+  def reverse[T](list: CList[T]): CList[T] = {
+    if (list.isEmpty) Nil
+
+    @tailrec def go(reversedList: CList[T], currentList: CList[T]): CList[T] = {
+      if (currentList.isEmpty) return reversedList
+      go(push(reversedList, currentList.head), tail(currentList))
+    }
+
+    go(Nil, list)
+  }
+
+  def init[T](list: CList[T]): CList[T] = {
+    if (list.isEmpty) Nil
+
+    @tailrec def go(createdList: CList[T], currentList: CList[T]): CList[T] = {
+      if (currentList.isEmpty) Nil
+      if (currentList.tail == Nil) return reverse(createdList)
+      go(push(createdList, currentList.head), tail(currentList))
+    }
+
+    go(Nil, list)
   }
 
   def apply[T](nodes: T*): CList[T] = {
--- src/test/scala/com/jwcrain/scalacastle/CListTest.scala
+++ src/test/scala/com/jwcrain/scalacastle/CListTest.scala
@@ -15,4 +15,24 @@ class CListTest extends FunSuite {
     val list: CList[Double] = CList(4.0, 5.0, 6.0)
     assert(CList.product(list) == 120)
   }
+
+  test("tail of 1, 2, 3, 4, 5 should be 2, 3, 4, 5") {
+    val expected: CList[Int] = CList(2, 3, 4, 5)
+    assert(CList.tail(CList(1, 2, 3, 4, 5)) == expected )
+  }
+
+  test("dropping 3 elements from 1, 2, 3, 4, 5 should yield 4, 5") {
+    val expected: CList[Int] = CList(4, 5)
+    assert(CList.drop(CList(1, 2, 3, 4, 5), 3) == expected)
+  }
+
+  test("dropping even numbers from 6, 4, 2, 1, 2, 4, 6 should yield 1, 2, 4, 6") {
+    val expected: CList[Int] = CList(1, 2, 4, 6)
+    assert(CList.dropWhile(CList(6, 4, 2, 1, 2, 4, 6), (n: Int) => n % 2 == 0) == expected)
+  }
+
+  test("init 1, 2, 3 should yield 1, 2") {
+    val expected: CList[Int] = CList(1, 2)
+    assert(CList.init(CList(1, 2, 3)) == expected)
+  }
 }
